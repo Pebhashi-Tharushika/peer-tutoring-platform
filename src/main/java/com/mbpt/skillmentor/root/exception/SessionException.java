6c4010e661ebb0c4bc57f91e1a44c875be279bf0
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-27 16:18:45 +0530

    create custom exception classes

---
+++ src/main/java/com/mbpt/skillmentor/root/exception/SessionException.java
@@ -0,0 +1,7 @@
+package com.mbpt.skillmentor.root.exception;
+
+public class SessionException extends Exception {
+    public SessionException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
