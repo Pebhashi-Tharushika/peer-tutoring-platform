ce1dfbadf97a2c9a9fc1d1f71ac7486e210c831c
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-04 07:55:07 +0530

    access environment variable through @Value

--- src/main/java/com/mbpt/skillmentor/root/service/impl/StudentServiceImpl.java
+++ src/main/java/com/mbpt/skillmentor/root/service/impl/StudentServiceImpl.java
@@ -8,6 +8,7 @@ import com.mbpt.skillmentor.root.repository.StudentRepository;
 import com.mbpt.skillmentor.root.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -19,6 +20,9 @@ import java.util.List;
 @Slf4j
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    @Value("${spring.datasource.url}")
+    private String datasource;
 
     @Autowired
     private StudentRepository studentRepository;
@@ -35,7 +39,7 @@ public class StudentServiceImpl implements StudentService {
         log.debug("StudentDTO received: {}", studentDTO);
         final StudentEntity studentEntity = StudentEntityDTOMapper.map(studentDTO);
         final StudentEntity savedEntity = studentRepository.save(studentEntity);
-        log.info("Student created with ID: {}", savedEntity.getStudentId());
+        log.info("Student created with ID: {} at data-source: {}", savedEntity.getStudentId(), this.datasource);
         return StudentEntityDTOMapper.map(savedEntity);
     }
 
@@ -55,7 +59,7 @@ public class StudentServiceImpl implements StudentService {
                 .filter(student -> firstNames == null || firstNames.contains(student.getFirstName()))
                 .map(StudentEntityDTOMapper::map)
                 .toList();
-        log.info("Found {} students after filtering", studentDTOList.size());
+        log.info("Found {} students after filtering from data-source: {}", studentDTOList.size(), this.datasource);
         return studentDTOList;
     }
 
@@ -70,7 +74,7 @@ public class StudentServiceImpl implements StudentService {
                     return StudentEntityDTOMapper.map(student);
                 })
                 .orElseThrow(() -> {
-                    log.error("Student not found with ID: {}", id);
+                    log.error("Student not found with ID: {} from data-source:{}", id, this.datasource);
                     return new StudentException("Student not found with ID: " + id);
                 });
     }
