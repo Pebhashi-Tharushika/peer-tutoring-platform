7fb57361c7d82d2274bb5a5e8a712674f3d9c942
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-28 21:31:49 +0530

    integrate Clerk Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Calendar } from "@/components/ui/calendar"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { useState } from "react"
 
 function App() {
   const [date, setDate] = useState<Date>();
   return (
     <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <Calendar
         mode="single"
         selected={date}
