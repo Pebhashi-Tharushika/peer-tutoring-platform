85be6e0031c11f124c5ab39d771644afbb744e22
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-28 16:19:03 +0530

    install and configure shadcn ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1 className="underline font-bold bg-amber-600 text-red-600 p-4">Peer Tutoring Platform</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
