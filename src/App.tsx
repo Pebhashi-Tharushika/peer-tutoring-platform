7f26d3bfd51e8f9e5839e8f9004b7d39bac29623
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-28 16:04:02 +0530

    install and config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>Peer Tutoring Platform</h1>
+      <h1 className="underline font-bold bg-amber-600 text-red-600 p-4">Peer Tutoring Platform</h1>
     </>
   )
 }
